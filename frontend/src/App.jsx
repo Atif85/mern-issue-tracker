95e7948045afa8fbfd6daf2b43a33771c4ba6188
Atif85 <atif58136@gmail.com>
2026-06-24 19:34:16 +0500

    Install tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,26 @@
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import toast from 'react-hot-toast';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <button
+        onClick={() => toast.success('Done')}
+        className="text-red-50 p-4 bg-blue-300"
+      >
+        Test Toast
+      </button>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <div>HomaPage</div>;
+};
+
+export default HomePage;
