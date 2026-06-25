5a112ff5b2003c3dae5f222e69addd590c3e3762
Atif85 <atif58136@gmail.com>
2026-06-25 19:23:09 +0500

    Prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ dns.setServers(['1.1.1.1', '1.0.0.1']);
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
@@ -12,13 +13,27 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
+if (process.env.NODE_ENV !== 'development') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173/',
+    })
+  );
+}
+
 app.use(express.json());
 app.use('/api/notes', notesRoutes);
-app.use(cors({
-  origin: "http://localhost:5173/"
-}));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
